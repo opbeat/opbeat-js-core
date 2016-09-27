c631d5788d6a7bd568a737dab684bc381fdbac75
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2016-09-27 12:04:21 +0200

    feat: expose public APIs

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// export public core APIs.
+
+module.exports['ServiceFactory'] = require('./common/serviceFactory')
+module.exports['ServiceContainer'] = require('./common/serviceContainer')
+module.exports['ConfigService'] = require('./lib/config')
+module.exports['TransactionService'] = require('./transaction/transaction_service')
+
+module.exports['patchUtils'] = require('./common/patchUtils')
+module.exports['patchCommon'] = require('./common/patchCommon')
+module.exports['utils'] = require('./lib/utils')
