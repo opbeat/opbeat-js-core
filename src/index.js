f320bb2ca9a95976b4e460ecd42028551d00da13
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2016-09-27 12:58:33 +0200

    feat: expose Subscription

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ module.exports['ServiceFactory'] = require('./common/serviceFactory')
 module.exports['ServiceContainer'] = require('./common/serviceContainer')
 module.exports['ConfigService'] = require('./lib/config')
 module.exports['TransactionService'] = require('./transaction/transaction_service')
+module.exports['Subscription'] = require('./common/subscription')
 
 module.exports['patchUtils'] = require('./common/patchUtils')
 module.exports['patchCommon'] = require('./common/patchCommon')
