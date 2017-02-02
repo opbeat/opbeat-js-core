f3eda4fdce7fd7c912b8b6c74ded3ac8cce2ca4b
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2017-02-02 13:04:08 +0100

    initial port of fetchPatch

--- src/common/patchCommon.js
+++ src/common/patchCommon.js
@@ -1,7 +1,9 @@
 var patchXMLHttpRequest = require('./patches/xhrPatch')
+var patchFetch = require('./patches/fetchPatch').patchFetch
 
 function patchCommon (serviceContainer) {
   patchXMLHttpRequest(serviceContainer)
+  patchFetch()
 }
 
 module.exports = patchCommon
