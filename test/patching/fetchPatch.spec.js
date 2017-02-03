417429674e310d03042826d2ad279d624802d8ab
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2017-02-03 13:15:12 +0100

    fix testing fetch on unsupported platforms

--- test/patching/fetchPatch.spec.js
+++ test/patching/fetchPatch.spec.js
@@ -5,9 +5,27 @@ describe('fetchPatch', function () {
   var serviceFactory = new ServiceFactory()
   var performance = serviceFactory.getPerformanceServiceContainer()
   performance.initialize()
+
+  var originalFetch = window.fetch
   patchFetch(performance)
 
+  function isPlatformSupported () {
+    return !!originalFetch
+  }
+
+  it('should check if fetch is supported before patching', function () {
+    if (isPlatformSupported()) {
+      expect(typeof window.fetch).toBe('function')
+    } else {
+      expect(window.fetch).toBe(originalFetch)
+    }
+  })
+
   it('should fetch out of opbeat zone', function (done) {
+    if (!isPlatformSupported()) {
+      done()
+      return
+    }
     window.fetch(window.location.href).then(function (response) {
       expect(response.status).toBe(200)
       done()
@@ -17,6 +35,10 @@ describe('fetchPatch', function () {
   })
 
   it('should fetch within the opbeat zone', function (done) {
+    if (!isPlatformSupported()) {
+      done()
+      return
+    }
     performance.services.zoneService.runInOpbeatZone(function () {
       window.fetch(window.location.href).then(function (response) {
         expect(response.status).toBe(200)
